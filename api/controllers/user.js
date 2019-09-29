ba23c3618bdf3f68a3d6454916b23fabc3992a54
Carlos Gonzalez <carlosfelipe20082008@hotmail.com>
2019-09-28 22:47:17 -0500

    Actualizacion de datos de usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -130,6 +130,21 @@ function getUsers(req, res){
         });
     });
 }
+function updateUser(req, res){
+   var userId= req.params.id;//recoger id de la url
+   var update=req.body;
+   delete update.password;//borrar propiedad password
+   if(userId != req.user.sub){
+         return res.status(500).send({message:'No tienes permiso para actualizar los datos del usuario'});
+        }
+      User.findByIdAndUpdate(userId, update, {new:true} ,(err, userUpdate)=>{
+               if(err) return res.status(500).send({message: 'Error en la peticion'});
+               
+               if(!userUpdate) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+               
+               return res.status(200).send({user: userUpdate});
+            });
+}
 
 module.exports={
     home,
@@ -137,5 +152,6 @@ module.exports={
     saveUser,
     loginUser,
     getUser,
-    getUsers
+    getUsers,
+    updateUser
 }
