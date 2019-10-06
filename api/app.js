0f0113c0a87484a24d1d6f169eb5675eb637509b
Carlos Gonzalez <carlosfelipe20082008@hotmail.com>
2019-10-06 11:05:33 -0500

    Metodos en Controlador de Follows

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var app = express();
 
 //cargar rutas
 var user_routes = require('./routes/user');
+var follow_routes = require('./routes/follow');
 
 // middlewares (metodo que se ejecuta antes de que llegue a un controlador)
 app.use(bodyParser.urlencoded({ extended:false}));
@@ -17,6 +18,7 @@ app.use(bodyParser.json());//convierto a json
 
 //rutas
 app.use('/api',user_routes);//middleware
+app.use('/api',follow_routes);
 
 //exportar
 module.exports =app;
---
+++ api/controllers/follow.js
@@ -0,0 +1,73 @@
+'use strict'
+
+//var path = require(path);
+//var fs=require('fs');
+var mongoosePaginate = require('mongoose-pagination');
+
+var User = require('../models/user');
+var Follow = require('../models/follow');
+
+function saveFollow(req, res){
+    var params = req.body;
+    var follow = new Follow();
+    follow.user =  req.user.sub; // seguidor
+    follow.followed = params.followed // seguido
+
+    follow.save((err,followStored) => {
+        if(err) return res.status(500).send({message : 'Error al guardar el seguimiento'});
+        if(!followStored){
+            return res.status(404).send({message : 'El seguimiento no se ha guardado'});
+        }
+        return res.status(200).send({follow : followStored});
+    });
+}
+
+function deleteFollow(req, res){
+        var userId = req.user.sub;
+        var followId = req.params.id;
+
+        Follow.find({'user':userId, 'followed':followId}).remove(err =>{
+            if(err) return res.status(500).send({message : 'Error al dejar de seguir'});
+
+            return res.status(200).send({message:'Follow eliminado correctamente'});
+        });
+}
+
+//listar los usuarios que esta siguiendo determinado usuario
+function getFollowingUsers(req, res){
+    var userId = req.user.sub;
+    if(req.params.id&&req.params.page){
+        userId=req.params.id;
+    }
+
+    var page=1;
+
+    if(req.params.page){
+        page=req.params.page;
+    }else if(req.params.id){
+        page=req.params.id; 
+    }
+
+    var itemsPerPage = 4 ;
+
+    console.log(page);
+    Follow.find({user:userId}).populate({path: 'followed'}).paginate(page,itemsPerPage,(err, follows, total)=>{//el populate es para mostrar la informacion interna de el usuario seguido, en vez de mostrar el id solamente
+        console.log(page);
+        if(err) return res.status(500).send({message : 'Error en el servidor'});
+        if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningun usuario'});
+        return res.status(200).send({
+            total : total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+
+
+
+}
+
+module.exports={
+     saveFollow,
+     deleteFollow,
+     getFollowingUsers
+}
