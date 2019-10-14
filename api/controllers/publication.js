e56a893445ff9f1fd0d4c04e3d4c0b65ff3f3013
carlos <carlosfelipe20082008@hotmail.com>
2019-10-14 12:56:33 -0500

    Metodo Get Publications

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -28,8 +28,44 @@
           
           return res.status(200).send({publication: publicationStored});
         });
+ }
+
+ function getPublications(req, res){
+    var page=1; 
+    if(req.params.page){
+        page=req.params.page;
+     }
+
+     var itemsPerPage=4;
+
+     Follow.find({user:req.user.sub}).populate('followed').exec((err, follows)=>{
+        if(err) return res.status(500).send({message:'Error devolver el seguimiento'});
+        
+        var follows_clean = [];
+        follows.forEach((follow) =>{
+           follows_clean.push(follow.followed);
+        });
+        
+        Publication.find({user: {"in":follows_clean}}).sort('-created-at').populate('user').paginate(page, itemsPerPage, (err,publications, total)=>{
+            if(err) return res.status(500).send({message:'Error devolver publicacion'});
+            
+            if(!publications){
+                return res.status(404).send({message:'No hay publicaciones'});
+            }
+
+            return res.status(200).send({
+                total_items:total,
+                pages: Math.ceil(total/itemsPerPage),
+                page:page,
+                publications
+            });
+        
+        });//in es para buscar en un array
+
+    });
 
  }
  module.exports={
-     savePublication
+     savePublication,
+     getPublications
  }
--- api/routes/publication.js
+++ api/routes/publication.js
@@ -1,13 +1,14 @@
 'use strict'
-
 var express=require('express');
 var PublicationController= require('../controllers/publication');
-var api = express.Router;
+var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 var multipart= require('connect-multiparty');
 var md_upload = multipart({uploadDir:'./uploads/publications'});
 
 api.post('publication',md_auth.ensureAuth, PublicationController.savePublication);
+api.get('/publication/:page?',md_auth.ensureAuth,PublicationController.getPublications);
+
 
 module.exports=api;
