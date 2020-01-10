d4542c1bdd84e53b1567db914cee51e474335cb1
carlos <carlosfelipe20082008@hotmail.com>
2020-01-09 19:43:03 -0500

    retomando proyecto

--- api/app.js
+++ api/app.js
@@ -16,14 +16,12 @@ var message_routes=require('./routes/message');
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());//convierto a json
 
-//cors
-// configurar cabeceras http
+//cors-configurar cabeceras http
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
- 
     next();
 });
 
